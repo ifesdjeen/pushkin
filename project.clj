46424cd6c2ca79146652c1128b2e3aabfb609b65
Alex Petrov <alexp@coffeenco.de>
2013-08-29 14:38:24 +0200

    Update deps

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject ifesdjeen.clj-pusher "0.1.1-SNAPSHOT"
+(defproject ifesdjeen.clj-pusher "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clojurewerkz/eep "0.5.2"]
+                 [clojurewerkz/eep "1.0.0-alpha2"]
                  [aleph "0.3.0-rc2"]
                  [ring/ring "1.2.0"]
                  [ring-reload-modified "0.1.1"]
