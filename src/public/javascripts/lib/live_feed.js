bba9f92bbb957a2ef30d2f76361f70b362ab6d4d
Alex Petrov <alexp@coffeenco.de>
2014-02-13 17:42:49 +0100

    Make test vars in live_feed.js global

--- src/public/javascripts/lib/live_feed.js
+++ src/public/javascripts/lib/live_feed.js
@@ -1,5 +1,6 @@
+var channel, pusher;
 window.onload = function() {
-  var channel, pusher;
+
   pusher = new Pusher('abc123');
   channel = pusher.subscribe('my-channel');
   return channel.bind('test-event', function(data) {
